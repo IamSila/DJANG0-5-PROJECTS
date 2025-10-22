cc4a790cad77e1459e0ada70f6d74116db3dddd2
IamSila <silamulingi@gmail.com>
2025-10-22 03:25:25 +0300

    modified the admin page

--- blog_application/mysite/blog/models.py
+++ blog_application/mysite/blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         max_length=2,
         choices = Status,
