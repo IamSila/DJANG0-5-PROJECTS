77fc8afafeadf1f45ae144bee23063e7a57e5893
IamSila <silamulingi@gmail.com>
2025-11-03 16:52:29 +0300

    creating list and detail view

--- blog_application/mysite/blog/models.py
+++ blog_application/mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -21,8 +25,12 @@ class Post(models.Model):
         choices = Status,
         default = Status.DRAFT
     )
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish']),]
     def __str__():
         return self.title
+
+   
