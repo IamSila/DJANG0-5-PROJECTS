a5043375b6e2346ce323d28dcc5828518cb05f3f
IamSila <silamulingi@gmail.com>
2025-11-30 12:29:35 +0300

    pagination is cool

--- blog_application/mysite/blog/views.py
+++ blog_application/mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
@@ -6,16 +7,22 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts":posts})
+
+    posts_list = Post.published.all()
+    # pagination with 3 posts per page
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.page(page_number)
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request,year, month, day, post):
-    post = get_object_or_404(Post,
-                            status=Post.Status.PUBLISHED,
-                            publish__year = year,
-                            publish__month = month,
-                            publish__day = day,
-                            slug = post
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        slug=post,
     )
     return render(request, "blog/post/detail.html", {"post": post})
