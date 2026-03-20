d1559177b1cd04bcc246e4da11897fb9724e66d2
IamSila <silamulingi@gmail.com>
2026-03-21 01:43:35 +0300

    Tags

--- blog_application/mysite/blog/views.py
+++ blog_application/mysite/blog/views.py
@@ -25,8 +25,8 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag,slug=tag_slug)
         posts_list = posts_list.filter(tags__in=[tag])
     # pagination with 3 posts per page
-    paginator = Paginator(posts_list, 3)
-    page_number = request.GET.get("page", 10)
+    paginator = Paginator(posts_list, 10)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -34,7 +34,7 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag":tag})
 
 
 def post_detail(request, year, month, day, post):
