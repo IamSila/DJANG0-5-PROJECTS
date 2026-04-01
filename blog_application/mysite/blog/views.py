71b71bc90d21e215edf862af8ac1528fa7ffb7e8
IamSila <silamulingi@gmail.com>
2026-04-01 05:36:30 +0300

    Basic search engine for blog app

--- blog_application/mysite/blog/views.py
+++ blog_application/mysite/blog/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 # Create your views here.
 from .models import Post
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 
 class PostListView(ListView):
@@ -114,3 +115,21 @@ def post_comment(request, post_id):
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
     )
+
+
+
+#search view
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        results = (
+            Post.objects.annotate(
+                search = SearchVector('title', 'body'),
+            ).filter(search=query)
+        )
+    return render(request, 'blog/post/search.html', {'form':form, 'query':query, 'results':results})
